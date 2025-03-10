66452df2eb3abc7a9dccc59ced5ba920a1bc1be6
allmors <jesmora@protonmail.com>
2025-03-10 08:48:13 +0000

    up

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,8 @@
 import Fastify from "fastify";
 import routes from "./routes/index.mjs";
+import dotenv from 'dotenv';
+
+const env = dotenv.config().parsed;
 
 const server = Fastify({
     logger: true,
@@ -9,7 +12,7 @@ const server = Fastify({
 server.register(routes, { prefix: '/v1' })
 
 // 监听
-server.listen({ port: 8088, host: '0.0.0.0' }, function (err, address) {
+server.listen({ port: env.PORT || 56553, host: '0.0.0.0' }, function (err, address) {
     if (err) {
         server.log.error(err)
         process.exit(1)
